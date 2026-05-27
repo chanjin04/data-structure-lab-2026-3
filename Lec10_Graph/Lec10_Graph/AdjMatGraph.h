f46f0eb2b25f3b0df637e1267e724b2652d19dc8
chanjin04 <chanjin2451@naver.com>
2026-05-27 13:40:40 +0900

    가중치 수정

--- Lec10_Graph/Lec10_Graph/AdjMatGraph.h
+++ Lec10_Graph/Lec10_Graph/AdjMatGraph.h
@@ -77,22 +77,23 @@ public:
 	}bool hasEdge(int i, int j) {
 		return (getEdge(i, j) < INF);
 	}
-	void load(char* filename) {
-		FILE* fp = fopen(filename, "r");
+	void load(const char* filename) {
+		FILE* fp;
+		fopen_s(&fp, filename, "r");
 		if (fp != NULL) {
 			int n, val;
-			fscanf(fp, "%d", &n);
+			fscanf_s(fp, "%d", &n);
 			for (int i = 0; i < n;i++) {
 				char str[80];
 				int val;
-				fscanf(fp, "%s", str);
+				fscanf_s(fp, "%s", str, sizeof(str));
 				insertVertex(str[0]);
 				for (int j = 0;j < n;j++) {
-					fscanf(fp, "%d", &val);
+					fscanf_s(fp, "%d", &val);
 					insertEdge(i, j, val);
 				}
-				fclose(fp);
 			}
 		}
+		fclose(fp);
 	}
 };
